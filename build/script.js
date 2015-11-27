4d15fe1d5895d5a1fbf446a904160962bbc9d139
Eric Alas <alas.eric@gmail.com>
2015-11-27 14:48:28 -0500

    add bootstrap for styling, poems can now be submitted through textarea, fixed bug where undefined is displayed

--- build/script.js
+++ build/script.js
@@ -1,31 +1,28 @@
 let poemSubmitter = document.getElementById('poem-input');
 let poemLinesWrapper = document.getElementById('poem-lines');
-let poemLinesChildren = poemLinesWrapper.querySelectorAll('span');
 
 let addAnotherPoemBtn = document.getElementById('add-poem');
 let submitButton = document.getElementById('submit-poems');
 
-let poemOne = "Hey there + my name is + jonas mccoolster + whats yours?";
-let poemTwo = "Greetings + i am called + james corvich + and your title is?";
-let poemThree = "Salutations + my birth name is + eric alas + its a pleasure to meet you...";
-let poemFour =  "Hello there sailor + i am known as + usidore the blue + do you have a name?";
-
 let submittedPoems = [];
-
-let numberOfPoems = 4;
-
-//this is how many poems there are
-let poems = new Array(numberOfPoems);
-
-//each poem gets its own color
-let styles = {
-	0: 'red', 
-	1: 'green', 
-	2: 'orange', 
-	3: 'blue', 
-};
+let numberOfPoems = 0;
+let poems = [];
 
 
+/*
+	This function takes all the submitted poems, splits them and stores them into a 1D array
+	It then takes the 1D array. and adds them (the splitted poems) and stores them into a 2D array
+	ex:
+	
+	index|	0	|	1	|	2	|
+	-----|------------------------
+	0	 |poem1 |poem1	|poem1	|
+		 |line1	|line2	|line3	|
+	-----|------------------------
+	1	 |poem2 |poem2	|poem2	|
+		 |line1	|line2	|line3	|
+		 
+*/
 function splitPoems(){
 	let poemsSplit = [];
 
@@ -33,68 +30,165 @@ function splitPoems(){
 		poemsSplit.push(submittedPoems[poemNumber].split(" + "))
 	}
 	
- 	for(let j = 0 ; j < poemsSplit.length; ++j){
-	 	poems[j] = new Array(numberOfPoems)
-	 	
-	 	for(let k = 0; k < poemsSplit[j].length; ++k){
-		 	poems[j][k] = poemsSplit[k][j]
+ 	for(let j = 0 ; j < numberOfPoems; ++j){
+	 	let poemSplitLength = poemsSplit[j].length;
+	 	poems[j] = new Array(poemSplitLength)
+	 	for(let k = 0; k < poemSplitLength; ++k){
+		 	let temp  = poemsSplit[j][k];
+ 		 	poems[j][k] = temp
 	 	}
  	}
 
+}
 
-};
+/*
+	this function finds the max number of lines in all the poems
+	one poem may have 4 lines, and the next may have 5, and so on
+	it'll return the max number of lines of all the poems (ex, 5)
+*/
 
-function initilizePoems(){
+function findMaxLinesInPoem(){
+	let max = poems[0].length;
+	for(let i = 0; i < numberOfPoems; ++i){
+		if(max < poems[i].length)
+			max = poems[i].length
+	}
+	return max;
+}
+
+/*
+	this function finds the max number of lines from function findMaxLinesInPoem, 
+	creates span elements to display the lines of the poems
+*/
+
+function initilizeHTML(){
+	let maxLines = findMaxLinesInPoem();
+	
+	for(let i = 0; i < maxLines ; ++i){
+		let newSpan = document.createElement("span");
+		newSpan.setAttribute("data-line", `line-${i+1}`);
+		poemLinesWrapper.appendChild(newSpan);
+		poemLinesWrapper.appendChild(document.createElement("br"))
+	}
+	
+}
+
+/*
+	this function takes each span element created in initilizeHTML, 
+	adds an event handler based on the data-line attribute, 
+	and places the first poem inside each span,
+	if the max lines is greater than the number of lines in the first poem, 
+	print an elipses (...)
+*/
+
+function initilizePoems(){	
 	//hide poem input box to begin intilizing the poems
 	//using jQuery for browser support
-	$("#poem-input").hide();
-	$("#poem-lines").show();
+	$("#poem-input").fadeOut(1000, function(){
+		$("#poem-lines-wrapper").fadeIn(0, function(){
+			$("#poem-lines").fadeIn();
+		});
+	});
 	
-	for(let i = 0; i < poemLinesChildren.length; ++i){
+	let maxLines = findMaxLinesInPoem();
+	let poemLinesChildren = poemLinesWrapper.querySelectorAll('span');
+
+	for(let i = 0; i < maxLines; ++i){
 		let poemLineNumber = poemLinesChildren[i].getAttribute("data-line");
-		poemLinesChildren[i].addEventListener("mouseover", hoverHandler(poemLineNumber));
-		poemLinesChildren[i].style.color = styles[0];
-		poemLinesChildren[i].innerHTML = poems[i][0];
+		poemLinesChildren[i].addEventListener("mouseover", hoverHandler(i), false);
+		poemLinesChildren[i].style.color = "#3f3f3f";
+		
+		if(poems[0][i] === undefined)
+			poemLinesChildren[i].innerHTML = "...";
+		else
+			poemLinesChildren[i].innerHTML = poems[0][i];
 	}
-};
+}
+
+/*
+	as the user hovers over different poems, this functions
+	changes the color a shade lighter so that the user can see
+	the changes in poem lines
+*/
+
+function shadeColor(color, shade) {
+    let colorInt = parseInt(color.substring(1),16);
+
+    let R = (colorInt & 0xFF0000) >> 16;
+    let G = (colorInt & 0x00FF00) >> 8;
+    let B = (colorInt & 0x0000FF) >> 0;
+
+    R = R + Math.floor((shade/255)*R);
+    G = G + Math.floor((shade/255)*G);
+    B = B + Math.floor((shade/255)*B);
+
+    let newColorInt = (R<<16) + (G<<8) + (B);
+    let newColorStr = `#${newColorInt.toString(16)}`;
+
+    return newColorStr;
+}
+
+/*
+	this functions uses closure to store information for each event listener
+	it iterates though the 2D array through colums, not rows, displaying the line
+	of each poem as the user hovers over them.
+	if they reach the first line in the poem, (first if condition) return to first poem
+	index, and restore default color.
+	Similar to initilizePoems, if there is a blank (due to shortage of lines in poem) 
+	display elipsis (...)
+*/
 
 var hoverHandler = function(poemLineNumber){
-	let lineNumber = poemLineNumber - 1;
-	let poemLine = 0;
+	let lineNumber = 0;
+	let poemLine = poemLineNumber;
+	let defaultColor = "#3f3f3f";
+	let style = defaultColor;
 	
 	function incrementNextPoemLine(){	
+		lineNumber++;
+		style = shadeColor(style, +100)
+		console.log(style)
+
+		if( (lineNumber) ===  numberOfPoems ){
+			lineNumber = 0;
+			style = defaultColor;
+		}
 		
-		if( (poemLine + 1) ===  poems[lineNumber].length || poems[lineNumber][poemLine + 1 ] === undefined)	
-			poemLine = 0;
+		this.style.color = style;
+		if(poems[lineNumber][poemLine] ===  undefined)
+			this.innerHTML = '...'
 		else
-			poemLine++;
-			
-		this.style.color = styles[poemLine]
- 		this.innerHTML = poems[lineNumber][poemLine]
-		
+	 		this.innerHTML = poems[lineNumber][poemLine]
 	}
 	
 	return incrementNextPoemLine;
 };
 
-
+/*
+	init function is called as soon as page is loaded, 
+	hides certain elements, adds an event listener to two
+	buttons, add poem and submit poems
+*/
 function init(){
 	//hide the poem display
-	$("#poem-lines").hide();
-	
+	$("#poem-lines, #poem-lines-wrapper").hide();
 	
 	let poem = document.getElementById('poem-writing');
-	
 	
 	addAnotherPoemBtn.addEventListener("click", () => {
 		submittedPoems.push(poem.value);
 		poem.value = "";
+		numberOfPoems++;
 	});
 	
 	submitButton.addEventListener("click", () => {
-		submittedPoems.push(poem.value);
+		if(poem.value !== ""){
+			submittedPoems.push(poem.value);
+			numberOfPoems++;
+		}
 		poem.value = "";
 		splitPoems();
+		initilizeHTML();
 		initilizePoems();
 	});
 	
